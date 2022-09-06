14610019b617bf8f4ddeb614aa8f6e8440d43f9e
silvercry007 <shubhamrules007@gmail.com>
2022-09-07 04:35:56 +0530

    Math (A/S/M/D)

---
+++ Math.py
@@ -0,0 +1,10 @@
+a = float(input("Enter the value of A - "))
+b = float(input("Enter the value of B - "))
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
