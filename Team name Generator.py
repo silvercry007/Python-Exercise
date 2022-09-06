cb50be7c7e985dd8e0b5b2618768cfcd042df1f7
silvercry007 <shubhamrules007@gmail.com>
2022-09-06 10:13:44 +0530

    Team name Generator

---
+++ Team name Generator.py
@@ -0,0 +1,6 @@
+print ("Welcome to the Team name Generator")
+city = input("Which city do you live in ? - ")
+color = input("What is your favourite color ? - ")
+random = input("type a Random word that you like ")
+name = city + color + random
+print(name)
