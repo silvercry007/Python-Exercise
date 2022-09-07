d3aaabd10a20281ae57c0d93665cd99bbcbcbb75
silvercry007 <shubhamrules007@gmail.com>
2022-09-07 22:50:03 +0530

    Integer and Float Division

---
+++ Int and Float Divison.py
@@ -0,0 +1,6 @@
+a = int(input("Enter value of A - "))
+b = int(input("Enter value of B - "))
+c = int(a/b)
+d = float(a/b)
+print("The interger Divison is - " + str(c))
+print("The Float Division is - " + str(d))
