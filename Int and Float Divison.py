8b38a6b880baa595567e4e592367806cf215b1bc
silvercry007 <shubhamrules007@gmail.com>
2022-09-07 23:38:01 +0530

    Python executable for Bash

--- Int and Float Divison.py
+++ Int and Float Divison.py
@@ -1,6 +1,8 @@
+#!/c/Users/shubh/AppData/Local/Microsoft/WindowsApps/python
 a = int(input("Enter value of A - "))
 b = int(input("Enter value of B - "))
 c = int(a/b)
 d = float(a/b)
 print("The interger Divison is - " + str(c))
 print("The Float Division is - " + str(d))
+
