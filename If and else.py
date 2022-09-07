c382aded506d09576797b06335ec0f94c90f2e29
silvercry007 <shubhamrules007@gmail.com>
2022-09-08 02:59:06 +0530

    If else , Which Number is Greater

---
+++ If and else.py
@@ -0,0 +1,11 @@
+first_number = int(input("Enter First Number - "))
+second_number = int(input("Enter Second Number - "))
+if first_number > second_number:
+    print("First Number is Greater")
+elif first_number ==  second_number:
+    print("Numbers are Equal")
+elif first_number < second_number:
+    print("Second Number is Greater")
+else:
+    print ("Number is invalid")
+    
