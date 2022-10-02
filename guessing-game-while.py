2fbb6c1bf17d7cd9af47f6c2265cef1c8a30858d
silvercry007 <shubhamrules007@gmail.com>
2022-10-02 20:54:04 +0530

    Points Update

--- guessing-game-while.py
+++ guessing-game-while.py
@@ -2,18 +2,20 @@ import random
 
 i = random.randint(0, 9)
 tries = 3
+points = 100
 print(i)
 while (True):
     x = int(input("Enter a number : "))
     if (x==i):
-        print("This Is Your number ", x)
+        print("This Is Your number ", x , " You have ", points, " Points")
         break
     elif (x != i):
-        tries -= 1 
+        tries -= 1
+        if (points == 100):
+            points = points - 20
+        elif (points == 80):
+            points = points - 30
         print("It is not you number, you have " + str(tries) +  " tries left")
         if (tries == 0):
-            print("Bhaag idhr se")
+            print("Too bad, you earned 0 points. Better Luck next time")
             break
-    
-        
-        
