2241419608c95aefbbc8fab8438968c06dda682e
silvercry007 <shubhamrules007@gmail.com>
2022-10-23 13:16:19 +0530

    Quiz

--- quiz.py
+++ quiz.py
@@ -2,39 +2,40 @@ print("Welcome to my computer quiz!")
 
 playing = input("Are you ready to play the game? ")
 
-if playing.title() != "yes":
-    quit()
+if playing.title() != "Yes":
+    print("Let start...!! ")
 
-print("Let start...!! ")
-score = 0
+    score = 0
 
-answer = input("What is the capital of india?  ")
-if answer.title() == "New Delhi":
-    print('Correct!')
-    score += 1
+    answer = input("What is the capital of india?  ")
+    if answer.title() == "New Delhi":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
+
+    answer = input("How many states are in India ? ")
+    if answer.title() == "28":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
+
+    answer = input("How many Union Territories are in India ? ")
+    if answer.title() == "8":
+        print('Correct!')
+        score += 1
+    else:  
+        print("Incorrect!")
+
+    answer = input("What is the National animal of India ? ")
+    if answer.title() == "Tiger":
+        print('Correct!')
+        score += 1
+    else:
+        print("Incorrect!")
+
+    print("You got " + str(score) + " answers!")
+    print("Your score is " + str((score / 4) * 100) + "%.")
 else:
-    print("Incorrect!")
-
-answer = input("How many states are in India ? ")
-if answer.title() == "28":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("How many Union Territories are in India ? ")
-if answer.title() == "8":
-    print('Correct!')
-    score += 1
-else:  
-    print("Incorrect!")
-
-answer = input("What is the National animal of India ? ")
-if answer.title() == "Tiger":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-print("You got " + str(score) + " answers!")
-print("Your score is " + str((score / 4) * 100) + "%.")
+    quit()    
